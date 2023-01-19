c2c36726d922155d72376b99cb068e84ebe8d403
Jakehair <jahuckob@gmail.com>
2023-01-18 20:39:18 -0700

    Finished Learning03 project

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction f1 = new Fraction();
+        f1.SetTop(1);
+        f1.SetBottom(3);
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f1.GetBottom());
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        f1.SetTop(3);
+        f1.SetBottom(4);
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f1.GetBottom());
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        f1.SetTop(5);
+        f1.SetBottom(6);
+        Console.WriteLine(f1.GetTop());
+        Console.WriteLine(f1.GetBottom());
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
     }
 }
